813467356fbf12954104b36e3e48001e4080d73d
yuvaanparwaney <63131496+yuvaanparwaney@users.noreply.github.com>
2020-05-31 10:13:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(200,330,400,20,ground_options)
+  World.add(world,ground);
+
+holder = Bodies.rectangle(200,100,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
+}
+
+ball  = Bodies.circle(220,200,40,ball_options);
+World.add(world,ball);
+
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.004,
+  length : 100
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("White");
+}
+
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+
+  fill ("yellow");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y-30,85,10);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+fill("red");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,60);
+
+strokeWeight(8);
+stroke("green");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y-30)
+
+
+
+
+if(keyCode===32){
+ball.position.y = mouseY;
+ball.position.x = mouseX;
+}
+
+else if (keyCode === ENTER){
+ball.position.x = 200;
+
+}
+
+}
+
+
+
+
+
+
+
+
